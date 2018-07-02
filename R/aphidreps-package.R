61bd6ad2ce5eeb9932ce89289ad6b44074ed1297
Lucas Nell <lucas@lucasnell.com>
2018-07-02 16:43:46 -0500

    Converted to R package

---
+++ R/aphidreps-package.R
@@ -0,0 +1,17 @@
+#' The 'aphidreps' package.
+#' 
+#' @description A DESCRIPTION OF THE PACKAGE
+#' 
+#' @docType package
+#' @name aphidreps-package
+#' @aliases aphidreps
+#' @useDynLib aphidreps, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @import rstantools
+#' @importFrom rstan sampling
+#' 
+#' @references 
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.17.3. http://mc-stan.org
+#' 
+NULL
