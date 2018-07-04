bf1fd56be5d3de3dd5c731f15ee2931517a99b5a
Lucas Nell <lucas@lucasnell.com>
2018-07-04 15:07:30 -0500

    Began choose_priors.Rmd, added location arg to compare_priors fxn

--- R/plots.R
+++ R/plots.R
@@ -10,7 +10,7 @@
 #'
 #' @export
 #'
-compare_priors <- function(scale = 1, df_t = 2, xlim = c(-10, 10)) {
+compare_priors <- function(location = 0, scale = 1, df_t = 2, xlim = c(-10, 10)) {
     dt_loc_scale <- function(x, df, location, scale) {
         1/scale * dt((x - location)/scale, df)
     }
@@ -25,15 +25,16 @@ compare_priors <- function(scale = 1, df_t = 2, xlim = c(-10, 10)) {
     }
     ggplot2::ggplot(data.frame(x = xlim), ggplot2::aes(x)) +
         stat_dist("normal", size = 0.75, fun = dnorm,
-                  args = list(mean = 0, sd = scale)) +
+                  args = list(mean = location, sd = scale)) +
         stat_dist("student_t", size = 0.75, fun = dt_loc_scale,
-                  args = list(df = df_t, location = 0, scale = scale)) +
+                  args = list(df = df_t, location = location, scale = scale)) +
         stat_dist("cauchy", size = 0.75, linetype = 2, fun = dcauchy,
-                  args = list(location = 0, scale = scale)) +
+                  args = list(location = location, scale = scale)) +
         stat_dist("laplace", size = 0.75, linetype = 2, fun = dlaplace,
-                  args = list(location = 0, scale = scale)) +
-        stat_dist("product_normal", size = 0.75, linetype = 2, fun = dproduct_normal,
-                  args = list(scale = 1))
+                  args = list(location = location, scale = scale)) +
+        # stat_dist("product_normal", size = 0.75, linetype = 2, fun = dproduct_normal,
+        #           args = list(scale = 1))
+        NULL
 }
 
 
