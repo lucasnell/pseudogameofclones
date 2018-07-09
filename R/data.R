7299e580b9ebaf08efec63dd76707989347b9f38
Lucas Nell <lucas@lucasnell.com>
2018-07-09 11:29:51 -0500

    Added model description vignette, leslie as pkg data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+
+#' Leslie matrices for fast and slow aphid populations.
+#'
+#' @format A list with two 27 x 27 matrices.
+#'
+#' @source \url{https://onlinelibrary.wiley.com/doi/abs/10.1890/13-1933.1}
+#'
+#' @export
+#'
+"leslie"
+
+
