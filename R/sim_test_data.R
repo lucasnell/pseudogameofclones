191237065d2a0a82a24f785054f55557eb97bb86
Lucas Nell <lucas@lucasnell.com>
2018-07-09 09:43:31 -0500

    Depends on pkgs instead of tidyverse, R --> R0 in sim_* fxn
    pkgs = dplyr, tidyr, readr, purrr, tibble

--- R/sim_test_data.R
+++ R/sim_test_data.R
@@ -2,7 +2,7 @@
 
 #' Simulate log-transformed counts of aphid populations for one or more lines.
 #'
-#' @param R0 Vector of population growth rates for each line.
+#' @param R Vector of population growth rates for each line.
 #' @param alpha Vector of density dependencies for each line.
 #' @param n_reps Number of reps per aphid line.
 #' @param nobs_ts Number of observations per time series.
@@ -11,16 +11,16 @@
 #' @return A matrix with simulated log(N) through time.
 #' @export
 #'
-sim_lines <- function(R0, alpha, n_reps, nobs_ts, sigma_process = 0.1) {
+sim_lines <- function(R, alpha, n_reps, nobs_ts, sigma_process = 0.1) {
 
     # Number of aphid lines:
-    n_lines <- length(R0)
+    n_lines <- length(R)
     if (length(alpha) != n_lines) {
-        stop("\nIn sim_lines, R0 and alpha must be the same length.",
+        stop("\nIn sim_lines, R and alpha must be the same length.",
              call. = FALSE)
     }
     if (length(n_reps) != n_lines) {
-        stop("\nIn sim_lines, R0 and n_reps must be the same length.",
+        stop("\nIn sim_lines, R and n_reps must be the same length.",
              call. = FALSE)
     }
     # Aphid line per time series
@@ -33,7 +33,7 @@ sim_lines <- function(R0, alpha, n_reps, nobs_ts, sigma_process = 0.1) {
     # n_reps <- as.integer(round(runif(n_lines, 3, 7)))
     # # # observations per time series
     # nobs_ts <- as.integer(round(runif(N_ts, 10, 15)))
-    # R0 <- round(abs(rnorm(n_lines, 0.29, 0.003)), 5)
+    # R <- round(abs(rnorm(n_lines, 0.29, 0.003)), 5)
     # alpha <- round(abs(rnorm(n_lines, 0.002, 0.0003)), 5)
 
 
@@ -44,7 +44,7 @@ sim_lines <- function(R0, alpha, n_reps, nobs_ts, sigma_process = 0.1) {
     X[1,] <- log(runif(ncol(X), 15, 25))
     for (i in 1:N_ts) {
         for (t in 1:(nobs_ts[i]-1)) {
-            X[(t+1),i] <- X[t,i] + R0[line_ts[i]] *
+            X[(t+1),i] <- X[t,i] + R[line_ts[i]] *
                 (1 - alpha[line_ts[i]] * exp(X[t,i])) + rnorm(1, 0, sigma_process)
         }
     }
