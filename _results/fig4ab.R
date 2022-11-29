f80a12117e9546518a277bbc7abdc4e2df345dee
Lucas Nell <lucas@lucasnell.com>
2022-11-29 15:03:24 -0800

    Fixed fig 4ab code

--- _results/fig4ab.R
+++ _results/fig4ab.R
@@ -86,12 +86,13 @@ tol <- 1e-3
 disp.list <- c(.01*(1:25))
 disp.list <- c(.0001*(1:9), disp.list, .258 + .0001*(1:6), .26, .28)
 
-pick.disp <- 25
+pick.disp <- 15
 delta.list <- exp(.25*(-20:20))
 sim <- sim_experiments(clonal_lines = c(line_s, line_r),
                        alate_field_disp_p = disp.list[pick.disp],
                        extinct_N = 1e-10,
                        max_t = max_t, save_every = 1e0)
+sim0 <- sim
 
 # clone_eig(sim)
 
@@ -102,10 +103,9 @@ rerun_sims <- TRUE
 
 
 
-
+# NOTE: The starting conditions for each new disp value are based on the previous value of disp. This is necessary, because the stationary point is only locally stable. The most stable stationary point is near the middle of values of disp.list, so the 15th element was selected to start, and the range of disp.list is completed going in both directions from 15.
 
 if (rerun_sims) {
-
 
     df <- data.frame(disp = rep(disp.list, each = length(delta.list)),
                      delta = rep(delta.list, times = length(disp.list)),
@@ -116,130 +116,113 @@ if (rerun_sims) {
     counter <- 0
     verbose <- FALSE
 
-    # Takes ~6 min
-    new.sim <- sim
-    new.starts <- new.sim$all_info[[1]]
-    # for(count.disp in (pick.disp-1):1){
-    for(count.disp in pick.disp:length(disp.list)){
-
-        i.disp <- disp.list[count.disp]
-        if (verbose) show(i.disp)
-
-        old.sim <- new.sim
-        new.sim <- restart_experiment(old.sim, new_starts = new.starts,
-                                      alate_field_disp_p = i.disp, max_t = max_t)
+    for(case in 1:2){
+        if(case == 1) {
+            count.list <- pick.disp:length(disp.list)
+        }else{
+            sim <- sim0
+            count.list <- (pick.disp-1):1
+        }
+        new.sim <- sim
         new.starts <- new.sim$all_info[[1]]
+        for(count.disp in count.list){
 
-        S.resistant <- sum(new.starts$N[!is.na(new.starts$line) &
-                                            new.starts$line == "resistant"])
-        S.susceptible <- sum(new.starts$N[!is.na(new.starts$line) &
-                                              new.starts$line == "susceptible"])
-        test <- (S.resistant/(S.resistant + S.susceptible) > tol)
-        if (!test) {
-            for(i.delta in rev(delta.list)) {
-                test.starts <- old.sim$all_info[[1]]
-                pick <- (!is.na(test.starts$line)) &
-                    (test.starts$line == "resistant")
-                test.starts[pick, "N"] <- i.delta * test.starts[pick, "N"]
+            i.disp <- disp.list[count.disp]
+            if (verbose) show(i.disp)
 
-                test.sim <- restart_experiment(old.sim, new_starts = test.starts,
-                                               alate_field_disp_p = i.disp,
-                                               max_t = max_t)
-                test.starts <- test.sim$all_info[[1]]
-                S.resistant <- sum(test.starts$N[(!is.na(test.starts$line)) &
-                                                     (test.starts$line == "resistant")])
-                S.susceptible <- sum(test.starts$N[(!is.na(test.starts$line)) &
-                                                       (test.starts$line == "susceptible")])
+            old.sim <- new.sim
+            new.sim <- restart_experiment(old.sim, new_starts = new.starts,
+                                          alate_field_disp_p = i.disp, max_t = max_t)
+            new.starts <- new.sim$all_info[[1]]
 
-                if (verbose) show(c(i.disp, i.delta, S.resistant/
-                                        (S.resistant + S.susceptible)))
+            S.resistant <- sum(new.starts$N[!is.na(new.starts$line) &
+                                                new.starts$line == "resistant"])
+            S.susceptible <- sum(new.starts$N[!is.na(new.starts$line) &
+                                                  new.starts$line == "susceptible"])
+            test <- (S.resistant/(S.resistant + S.susceptible) > tol)
+            if (!test) {
+                for(i.delta in rev(delta.list)) {
+                    test.starts <- old.sim$all_info[[1]]
+                    pick <- (!is.na(test.starts$line)) &
+                        (test.starts$line == "resistant")
+                    test.starts[pick, "N"] <- i.delta * test.starts[pick, "N"]
 
-                new.test <- (S.resistant/(S.resistant + S.susceptible) > tol)
-                if(new.test) break
+                    test.sim <- restart_experiment(old.sim, new_starts = test.starts,
+                                                   alate_field_disp_p = i.disp,
+                                                   max_t = max_t)
+                    test.starts <- test.sim$all_info[[1]]
+                    S.resistant <- sum(test.starts$N[(!is.na(test.starts$line)) &
+                                                         (test.starts$line == "resistant")])
+                    S.susceptible <- sum(test.starts$N[(!is.na(test.starts$line)) &
+                                                           (test.starts$line == "susceptible")])
+
+                    if (verbose) show(c(i.disp, i.delta, S.resistant/
+                                            (S.resistant + S.susceptible)))
+
+                    new.test <- (S.resistant/(S.resistant + S.susceptible) > tol)
+                    if(new.test) break
+                }
+                new.sim <- test.sim
+                new.starts <- test.sim$all_info[[1]]
             }
-            new.sim <- test.sim
-            new.starts <- test.sim$all_info[[1]]
+            S.resistant <- sum(new.starts$N[!is.na(new.starts$line) &
+                                                new.starts$line == "resistant"])
+            S.susceptible <- sum(new.starts$N[!is.na(new.starts$line) &
+                                                  new.starts$line == "susceptible"])
+
+            # aphids
+            d <- new.sim$aphids
+            d <- d[d$type != "mummy",]
+            d$line.type <- paste0(d$line,".", d$type)
+            d <- d[,-c(1,5,6)]
+            d <- spread(d, "line.type", "N")
+            d <- d[d$time >= 9000,]
+
+            d$resistant <- d$resistant.alate + d$resistant.apterous +
+                d$resistant.parasitized
+            d$susceptible <- d$susceptible.alate + d$susceptible.apterous +
+                d$susceptible.parasitized
+
+            d <- aggregate(cbind(resistant, susceptible) ~ time, data=d, FUN = sum)
+            d$prop <- d$resistant/(d$resistant + d$susceptible)
+
+            df$peak.prop[df$disp == i.disp] <- max(d$prop)
+            df$trough.prop[df$disp == i.disp] <- min(d$prop)
+            df$peak.resistant[df$disp == i.disp] <- max(d$resistant)
+            df$peak.susceptible[df$disp == i.disp] <- max(d$susceptible)
+            df$trough.resistant[df$disp == i.disp] <- min(d$resistant)
+            df$trough.susceptible[df$disp == i.disp] <- min(d$susceptible)
+
+            # wasps
+            d <- new.sim$wasps
+            d <- d[d$time >= 9000,]
+
+            d <- aggregate(wasps ~ time, data=d, FUN = sum)
+            df$peak.wasps[df$disp == i.disp] <- max(d$wasps)
+            df$trough.wasps[df$disp == i.disp] <- min(d$wasps)
+
+            eig <- clone_eig(new.sim)$value
+            df$eig[df$disp == i.disp] <- eig
+
+            for(i.delta in delta.list) {
+                df$converge[df$disp == i.disp & df$delta == i.delta] <-
+                    clone_converge(new.sim, i.delta, max_t = max_t)
+                df$prop.delta[df$disp == i.disp & df$delta == i.delta] <-
+                    i.delta * sum(S.resistant) /
+                    (i.delta * sum(S.resistant) + sum(S.susceptible))
+            }
+            if (verbose) show(df[df$disp == i.disp,])
         }
-        S.resistant <- sum(new.starts$N[!is.na(new.starts$line) &
-                                            new.starts$line == "resistant"])
-        S.susceptible <- sum(new.starts$N[!is.na(new.starts$line) &
-                                              new.starts$line == "susceptible"])
-
-        # aphids
-        d <- new.sim$aphids
-        d <- d[d$type != "mummy",]
-        d$line.type <- paste0(d$line,".", d$type)
-        d <- d[,-c(1,5,6)]
-        d <- spread(d, "line.type", "N")
-        d <- d[d$time >= 9000,]
-
-        d$resistant <- d$resistant.alate + d$resistant.apterous +
-            d$resistant.parasitized
-        d$susceptible <- d$susceptible.alate + d$susceptible.apterous +
-            d$susceptible.parasitized
-
-        d <- aggregate(cbind(resistant, susceptible) ~ time, data=d, FUN = sum)
-        d$prop <- d$resistant/(d$resistant + d$susceptible)
-
-        df$peak.prop[df$disp == i.disp] <- max(d$prop)
-        df$trough.prop[df$disp == i.disp] <- min(d$prop)
-        df$peak.resistant[df$disp == i.disp] <- max(d$resistant)
-        df$peak.susceptible[df$disp == i.disp] <- max(d$susceptible)
-        df$trough.resistant[df$disp == i.disp] <- min(d$resistant)
-        df$trough.susceptible[df$disp == i.disp] <- min(d$susceptible)
-
-        # wasps
-        d <- new.sim$wasps
-        d <- d[d$time >= 9000,]
-
-        d <- aggregate(wasps ~ time, data=d, FUN = sum)
-        df$peak.wasps[df$disp == i.disp] <- max(d$wasps)
-        df$trough.wasps[df$disp == i.disp] <- min(d$wasps)
-
-        eig <- clone_eig(new.sim)$value
-        df$eig[df$disp == i.disp] <- eig
-
-        for(i.delta in delta.list) {
-            df$converge[df$disp == i.disp & df$delta == i.delta] <-
-                clone_converge(new.sim, i.delta, max_t = max_t)
-            df$prop.delta[df$disp == i.disp & df$delta == i.delta] <-
-                i.delta * sum(S.resistant) /
-                (i.delta * sum(S.resistant) + sum(S.susceptible))
-        }
-        if (verbose) show(df[df$disp == i.disp,])
     }
-
-    df.save <- df
-    df <- rbind(df, df.save)
-    # write.csv(df, "_results/df_Fig4ab.csv", row.names = FALSE)
-
-
 }
+# write.table(df, "df_Fig4ab_18Nov22.csv", sep=",", row.names=F)
 
-
-
-########################*
-# df <- read.csv("_results/df_Fig4ab.csv")
-
-
-# df2 <- read.csv("~/Stanford_Drive/UW/gameofclones/_drafts/2022-10-31_ARI/df_Fig4ab_26Oct22.csv")
-
-
-########################*
-# crude figure
-df$col.converge <- df$converge+1
-df$lty.eig <- 2 - (Im(df$eig) == 0)
-
-
-par(mfrow=c(1,1))
-plot(prop.delta ~ disp, data = df, col = col.converge, pch=3)
-
-points(prop.delta ~ disp, data = df[df$delta == 1 & df$lty.eig == 2 & !is.na(df$lty.eig),], lty = lty.eig)
-points(peak.prop ~ disp, data = df[df$delta == 1 & !is.na(df$lty.eig),], lty = lty.eig)
-points(trough.prop ~ disp, data = df[df$delta == 1 & !is.na(df$lty.eig),], lty = lty.eig)
 
 ########################*
 # pretty figure
+########################*
+
+# pre-processing
 w <- data.frame(disp = unique(df$disp), stable.equil1 = NA, stable.equil2 = NA, unstable.equil = NA, upper = NA)
 for(i.disp in rev(w$disp)){
     ww <- df[df$disp == i.disp,]
@@ -269,24 +252,12 @@ w$peak.susceptible <- log10(w$peak.susceptible + .0001)
 w$trough.wasps <- log10(w$trough.wasps + .0001)
 w$peak.wasps <- log10(w$peak.wasps + .0001)
 
-w.spline <- smooth.spline(x = w$disp, y = w$unstable.equil, df = 6)
-w$unstable.equil.spline <- predict(w.spline, w$disp)$y
+ww <- w[!is.na(w$unstable.equil),]
 
-w$stable.equil1[w$disp >= 0.2586] <- NA
-w$stable.equil2[w$disp >= 0.2586] <- NA
-w$peak.resistant[w$disp > 0.2586] <- NA
-w$trough.resistant[w$disp > 0.2586] <- NA
-w$unstable.equil.spline[w$disp >= 0.2586] <- NA
+ww.spline <- smooth.spline(x = ww$disp, y = ww$unstable.equil, df = 6)
+ww$unstable.equil.spline <- predict(ww.spline, ww$disp)$y
 
-
-#
-ww <- w[w$disp <= 0.2585,]
-
-ww <- rbind(ww, rep(NA, ncol(ww)), rep(NA, ncol(ww)))
-ww$disp[nrow(ww)-1] <- w$disp[w$disp == 0.2585]
-ww$upper[nrow(ww)-1] <- 0
-ww$disp[nrow(ww)] <- .28
-ww$upper[nrow(ww)] <- 0
+www <- w[w$disp <= 0.2585,]
 
 
 # ============================================================================*
@@ -304,18 +275,17 @@ lines(trough.susceptible ~ disp, data = w, col="blue", lwd = 2)
 lines(peak.wasps ~ disp, data = w, col="red", lwd = 2)
 lines(trough.wasps ~ disp, data = w, col="red", lwd = 2)
 
-w <- w[w$disp <= 0.2585,]
-plot(stable.equil1 ~ disp, data = w, typ="l", xlim = c(0,.28), ylim = c(0,1), ylab = "Proportion resistant", xlab = "Aphid dispersal")
-conf_bounds(x = w$disp, y.lower = w$unstable.equil.spline, y.upper = w$upper, col="lightgray")
+plot(stable.equil1 ~ disp, data = www, typ="l", xlim = c(0,.28), ylim = c(0,1), ylab = "Proportion resistant", xlab = "Aphid dispersal")
 
 conf_bounds(x = ww$disp, y.lower = ww$upper, y.upper = rep(1,nrow(ww)), col="lightgray")
-conf_bounds(x = w$disp, y.upper = w$unstable.equil.spline, y.lower = rep(0,nrow(w)), col="lightgray")
+conf_bounds(x = ww$disp, y.upper = ww$unstable.equil.spline, y.lower = rep(0,nrow(ww)), col="lightgray")
+conf_bounds(x = c(0.2584, .28), y.lower = c(0,0), y.upper = c(1,1), col="lightgray")
 
-lines(stable.equil1 ~ disp, data = w, lwd = 2, col="green")
-lines(stable.equil2 ~ disp, data = w, lwd = 2, col="green")
-points(stable.equil1 ~ disp, data = w[w$disp > .04 & w$disp <= .15,], col="green")
-points(stable.equil2 ~ disp, data = w[w$disp > .04 & w$disp <= .15,], col="green")
-lines(unstable.equil.spline ~ disp, data = w, col = "black", lwd = 2)
+lines(stable.equil1 ~ disp, data = www, lwd = 2, col="green")
+lines(stable.equil2 ~ disp, data = www, lwd = 2, col="green")
+points(stable.equil1 ~ disp, data = www[www$disp > .04 & www$disp <= .15,], col="green")
+points(stable.equil2 ~ disp, data = www[www$disp > .04 & www$disp <= .15,], col="green")
+lines(unstable.equil.spline ~ disp, data = ww, col = "black", lwd = 2)
 
 # dev.off()
 
