05896c633202e40d78ac2dbff974765a6219e6cc
Lucas Nell <lucnell@gmail.com>
2025-04-28 15:28:49 -0400

    Updated attractiveness-* scripts

--- _testing/attractiveness-abm.R
+++ _testing/attractiveness-abm.R
@@ -9,6 +9,8 @@
 library(tidyverse)
 library(gganimate)
 library(pseudogameofclones)
+library(viridisLite)
+library(ggtext)
 
 # number of threads:
 .n_threads <- max(1L, parallel::detectCores() - 2L)
@@ -25,47 +27,55 @@ if (interactive()) {
 
 
 
+# color palette for target types:
+type_pal <- c(viridis(3, begin = 0.1, end = 0.9)[c(1,3,2)], "gray80") |>
+    set_names(c("bacteria", "virus", "bacteria + virus", "none"))
+
+
+
 xs <- 101L; ys <- 101L
 
-target_xy <- crossing(x = 1:(xs-1L), y = 1:(ys-1L)) |>
-    as.matrix()
-# target_types <- as.integer((1:nrow(target_xy)) %% 2 == 0L) + 1L
-target_types <- lapply(1:(xs-1L), \(i) if (i %% 2L == 0) rep(1:2, (ys-1)/2)
-                       else rep(2:1, (ys-1)/2)) |>
-    do.call(what = c)
+target_sims <- target_type_sims(xs, ys,
+                                wt_mat = rbind(c(1, 1), c(1, 1)),
+                                n_samples = c(5000, 5000))
+#' Convert to form usable in `searcher_sims`, where it's required that
+#' there's only one type per location.
+#' This means we'll encode a new 4th type referring to both 1 & 2 (the 3rd type
+#' refers to neither virus nor bacteria so cannot be involved with multiples).
+target_types <- target_sims[["type"]] |>
+    map_int(\(x) {
+        if (length(x) == 1L) return(x)
+        else return(4L)
+    })
+# Same but with no bacteria
+# This means that type 2 (bacteria) is changed to type 3 (none) and
+# type 4 (both) to type 1 (virus)
+nb_target_types <- case_when(target_types == 4L ~ 1L,
+                             target_types == 2L ~ 3L,
+                             .default = target_types)
 
-# target_xy <- cbind(x = c(-3, -3), y = c(0, 0))
-# target_types <- 1:2
+#' Info for target types (in this order):
+#' 1. virus-infected
+#' 2. Pseudomonas-infected
+#' 3. none
+#' 4. both
+l_star <- list(v = 5,   b = 2,    n = 0.5)
+bias <-   list(v = 0.2, b = -0.8, n = 0.1)
+target_info <- list(l_star = list(l_star$v, l_star$b, l_star$n, c(l_star$v, l_star$b)),
+                    bias =   list(bias$v, bias$b, bias$n, c(bias$v, bias$b)),
+                    l_i = rep(0.2, 4L),
+                    n_stay = rep(5L, 4L),
+                    # n_stay = rep(1e6L, 4L),
+                    n_ignore = rep(1e6, 4L))
+target_info$n_ignore  <- rep(10L, 4L) # ceiling(2 * map_dbl(target_info$l_star, max) / 2)
 
-target_p <- as_tibble(target_xy) |>
-    mutate(type = factor(target_types,
-                         labels = c("virus", "bacteria"))) |>
-    ggplot(aes(x,y)) +
-    geom_hline(yintercept = c(0, xs), color = "gray70") +
-    geom_vline(xintercept = c(0, ys), color = "gray70") +
-    geom_point(aes(color = type), shape = 19, size = 1) +
-    scale_color_manual(NULL, values = c("dodgerblue", "firebrick")) +
-    coord_equal()
-# target_p
-
-# Info for target types: virus- and Pseudomonas-infected, respectively:
-target_info <- list(l_star = c(5, 1) |> as.list(),
-                    l_i = c(0.2, 0.2),
-                    bias = c(0.3, -0.6) |> as.list(),
-                    n_stay = c(5L, 5L),
-                    n_ignore = c(1e6, 1e6))
-target_info$n_ignore  <- ceiling(2 * unlist(target_info$l_star) / 0.5)
-
-# Change these if only two targets (used for testing)
-if (nrow(target_xy) == 2) {
-    target_info$l_star <- c(5, 5) |> as.list()
-    target_info$l_i <- c(1, 1)
-}
+.d <- 0.1
 
 
 # Simulations with bacteria:
-b_sims <- searcher_sims(d = 2, max_t = 100, x_size = xs, y_size = ys,
-                        target_xy = target_xy, target_types = target_types,
+b_sims <- searcher_sims(d = .d, max_t = 1000, x_size = xs, y_size = ys,
+                        target_xy = target_sims[,c("x","y")] |> as.matrix(),
+                        target_types = target_types,
                         l_star = target_info$l_star,
                         l_i = target_info$l_i,
                         bias = target_info$bias,
@@ -74,46 +84,80 @@ b_sims <- searcher_sims(d = 2, max_t = 100, x_size = xs, y_size = ys,
                         n_searchers = 100L,
                         n_threads = .n_threads)
 # no bacteria sims:
-nb_sims <- searcher_sims(d = 2, max_t = 100, x_size = xs, y_size = ys,
-                         target_xy = target_xy[target_types == 1,],
-                         target_types = target_types[target_types == 1],
-                         l_star = target_info$l_star[1],
-                         l_i = target_info$l_i[1],
-                         bias = target_info$bias[1],
-                         n_stay = target_info$n_stay[1],
-                         n_ignore = target_info$n_ignore[1],
+nb_sims <- searcher_sims(d = .d, max_t = 1000, x_size = xs, y_size = ys,
+                         target_xy = target_sims[,c("x","y")] |> as.matrix(),
+                         target_types = nb_target_types,
+                         l_star = target_info$l_star,
+                         l_i = target_info$l_i,
+                         bias = target_info$bias,
+                         n_stay = target_info$n_stay,
+                         n_ignore = target_info$n_ignore,
                          n_searchers = 100L,
                          n_threads = .n_threads)
 
 
-p <- b_sims |>
-    mutate(searcher = factor(searcher)) |>
-    # filter(t < 100) |>
-    ggplot(aes(x,y)) +
-    geom_hline(yintercept = c(0, xs), color = "gray70") +
-    geom_vline(xintercept = c(0, ys), color = "gray70") +
-    geom_point(data = as_tibble(target_xy) |>
-                   mutate(type = factor(target_types,
-                                        labels = c("virus", "bacteria"))),
-               aes(fill = type), shape = 21, size = 1, stroke = 0) +
-    geom_path(aes(color = searcher), linewidth = 1, alpha = 0.5) +
-    geom_point(aes(color = searcher), size = 2, alpha = 0.5) +
-    scale_color_viridis_d(NULL, guide = "none") +
-    scale_fill_manual(NULL, values = c("dodgerblue", "goldenrod"),
-                      guide = "none") +
-    coord_equal()
 
-anim <- p +
-    labs(title = "Time: {frame_along}") +
-    transition_reveal(time) +
-    ease_aes("linear")
+bind_rows(b_sims |> mutate(bact = "w"),
+          nb_sims |> mutate(bact = "wo")) |>
+    filter(type != 0L) |> # these are rows where searchers aren't on targets
+    mutate(bact = factor(bact, levels = c("w", "wo"),
+                         labels = c("*Pseudomonas*", "no *Pseudomonas*")),
+           infect = factor(type == 1L | type == 4L, levels = c(TRUE, FALSE),
+                           labels = c("infected", "uninfected"))) |>
+    group_by(searcher, bact, infect) |>
+    summarize(hit = sum(hit), .groups = "drop") |>
+    ggplot(aes(bact, hit, color = bact)) +
+    geom_vline(xintercept = 0, linewidth = 0.5, color = "gray70") +
+    geom_point(shape = 1, alpha = 0.25,
+               position = position_jitterdodge(jitter.width = 0.25,
+                                               dodge.width = 0.75)) +
+    stat_summary(geom = "point", fun = mean, size = 4, shape = 18,
+                 position = position_dodge(width = 0.75)) +
+    ylab("Number of hits on target") +
+    facet_wrap( ~ infect, ncol = 1) +
+    scale_color_viridis_d(NULL, guide = "none", option = "plasma", begin = 0.2, end = 0.8) +
+    theme(axis.title.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.text.x = element_markdown(color = "black", size = 10),
+          legend.text = element_markdown())
 
-# animate(anim, nframes = length(unique(b_sims$time)))
 
 
+
+# p <- b_sims |>
+#     mutate(searcher = factor(searcher)) |>
+#     # filter(t < 100) |>
+#     ggplot(aes(x,y)) +
+#     geom_hline(yintercept = c(0, xs), color = "gray70") +
+#     geom_vline(xintercept = c(0, ys), color = "gray70") +
+#     geom_point(data = target_sims |>
+#                               mutate(type = type |>
+#                                          map(\(x) c("bacteria", "virus", "none")[x]) |>
+#                                          map_chr(\(x) paste(x, collapse = " + ")) |>
+#                                          factor(levels = c("bacteria", "virus",
+#                                                            "bacteria + virus", "none"))),
+#                aes(fill = type), shape = 21, size = 1, stroke = 0) +
+#     geom_path(aes(color = searcher), linewidth = 1, alpha = 0.5) +
+#     geom_point(aes(color = searcher), size = 2, alpha = 0.5) +
+#     scale_color_viridis_d(NULL, guide = "none", option = "plasma") +
+#     scale_fill_manual(NULL, values = type_pal) +
+#     coord_equal() +
+#     guides(fill = guide_legend(override.aes = list(size = 3)))
+#
+#
+# # p
+#
+# anim <- p +
+#     labs(title = "Time: {frame_along}") +
+#     transition_reveal(time) +
+#     ease_aes("linear")
+#
+# # animate(anim, nframes = length(unique(b_sims$time)))
+#
+#
 # anim_save("~/Desktop/searchers.gif", anim, nframes = length(unique(b_sims$time)),
 #           height = 5, width = 5, units = "in", res = 300)
-
+#
 # b_sims |>
 #     filter(type > 0) |>
 #     mutate(type = factor(type, labels = c("virus", "bacteria"))) |>
@@ -129,25 +173,51 @@ anim <- p +
 
 
 
-bind_rows(b_sims |> mutate(bact = "w"),
-          nb_sims |> mutate(bact = "wo")) |>
-    mutate(bact = factor(bact, levels = c("w", "wo"))) |>
-    filter(type == 1) |>
-    group_by(searcher, bact) |>
-    summarize(on = n(), hit = sum(hit), .groups = "drop") |>
-    pivot_longer(on:hit) |>
-    ggplot(aes(bact, value)) +
-    geom_hline(yintercept = 0, linewidth = 0.5, color = "gray70") +
-    geom_jitter(width = 0.25, height = 0, shape = 1) +
-    ylab("Number of events") +
-    stat_summary(geom = "point", fun = mean, size = 3, color = "firebrick") +
-    facet_wrap(~ name, scales = "free_y")
 
 
 
-library(tidyverse)
-library(pseudogameofclones)
 
+# ========================================================*
+# Test searcher sims ----
+# ========================================================*
+
+
+test_sims <- searcher_sims(d = 2, max_t = 100, x_size = 11L, y_size = 11L,
+                           target_xy = rbind(c(2L, 5L), c(8L, 5L)),
+                           target_types = c(1, 2),
+                           l_star = list(c(5, 2), c(5, 2)),
+                           bias = list(c(0.3, -0.7), c(-0.3, 0.7)),
+                           l_i = c(0.5, 0.5),
+                           n_stay = c(5, 5),
+                           n_ignore = c(10, 10),
+                           n_searchers = 1L)
+p <- test_sims |>
+    mutate(searcher = factor(searcher)) |>
+    # filter(t < 100) |>
+    ggplot(aes(x,y)) +
+    geom_hline(yintercept = c(0, 11), color = "gray70") +
+    geom_vline(xintercept = c(0, 11), color = "gray70") +
+    geom_path(aes(color = searcher), linewidth = 1, alpha = 0.5) +
+    geom_point(aes(color = searcher), size = 2, alpha = 0.5) +
+    geom_point(data = tibble(type = factor(c(1, 2)),
+                             x = c(2, 8),
+                             y = c(5, 5)),
+               aes(fill = type), shape = 21, size = 5, stroke = 0) +
+    scale_color_viridis_d(NULL, guide = "none", option = "plasma", begin = 0.4) +
+    scale_fill_viridis_d(NULL, guide = "none", end = 0.5) +
+    # guides(fill = guide_legend(override.aes = list(size = 3))) +
+    coord_equal()
+
+# p
+
+
+anim <- p +
+    labs(title = "Time: {frame_along}") +
+    transition_reveal(time) +
+    ease_aes("linear")
+
+
+animate(anim, nframes = length(unique(test_sims$time)), fps = 5)
 
 
 
@@ -158,18 +228,22 @@ library(pseudogameofclones)
 # ========================================================*
 
 
+library(tidyverse)
+library(pseudogameofclones)
+
 
 x_size = 50L
 y_size = 50L
-corr = rbind(c(1, 1.2),
-             c(1.2, 2))
 n_samples <- c(1000, 1000)
 
 samp_df <- target_type_sims(x_size,
                  y_size,
-                 corr,
+                 wt_mat,
                  n_samples) |>
-    mutate(type = map_chr(type, \(x) paste(x, collapse = "_")))
+    mutate(type = type |>
+               map(\(x) c("bacteria", "virus", "none")[x]) |>
+               map_chr(\(x) paste(x, collapse = " + ")) |>
+               factor(levels = c("bacteria", "virus", "bacteria + virus", "none")))
 
 
 samp_df |>
@@ -180,17 +254,19 @@ samp_df |>
 
 samp_df |>
     filter(map_lgl(type, \(x) !is.null(x))) |>
-    mutate(type = map_chr(type, \(x) paste(sort(x), collapse = "_"))) |>
     ggplot(aes(x, y, color = type)) +
     geom_point() +
-    scale_color_viridis_d(begin = 0.1, end = 0.9) +
-    coord_equal(xlim = c(0, x_size), ylim = c(0, y_size))
+    scale_color_manual(NULL, values = type_pal) +
+    coord_equal(xlim = c(0, x_size), ylim = c(0, y_size)) +
+    theme(axis.title = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank())
 
 
 
 library(spdep)
 
-samp_listw <- cbind(samp_df$x, samp_df$y) |>
+samp_list <- cbind(samp_df$x, samp_df$y) |>
     knearneigh(k = 8) |>
     knn2nb() |>
     nb2listw(style="B")
@@ -203,4 +279,19 @@ joincount.multi(factor(samp_df$type), samp_listw)
 
 
 
-
+c("White Tail Bristletooth Tang
+(Ctenochaetus flavicauda)",
+"Two Spot Bristletooth Tang
+(Ctenochaetus binotatus)",
+"Bristletooth Tomini Tang
+(Ctenochaetus tominiensis)",
+"Kole Yellow Eye Tang
+(Ctenochaetus strigosus)",
+"Squaretail Bristletooth Tang
+(Ctenochaetus truncatus)") |>
+    str_split("\n\\(") |>
+    map_chr(\(x) x[[2]]) |>
+    str_remove_all("\\)") |>
+    str_split(" ") |>
+    map_chr(\(x) x[[2]]) |>
+    paste(collapse = "\n") |> cat()
