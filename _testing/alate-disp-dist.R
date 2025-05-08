c0af98a51d0fafd96af8f6d6f0c0ba427f472ae1
Lucas Nell <lucnell@gmail.com>
2025-05-08 09:37:46 -0400

    removed unused test code in alate-disp-dist.R

--- _testing/alate-disp-dist.R
+++ _testing/alate-disp-dist.R
@@ -14,188 +14,6 @@ if (interactive()) {
 
 
 
-{
-    t0 <- Sys.time()
-    target_sims <- target_type_sims(134L+1L, 134L+1L, #<< bc it sims locations 1:(*_size-1)
-                                    n_lands = 1,
-                                    n_threads = 1,
-                                    wt_mat = rbind(c(1, 1), c(1, 1)),
-                                    n_samples = rep(round(134L*134L * 0.5), 2))
-    t1 <- Sys.time()
-    print(t1 - t0); rm(t0, t1)
-}
-# 5.513737 secs
-
-
-
-n_samps <- c(100, 200)
-wt_mat <- rbind(c(2, 1),
-                c(1, 3))
-n_x = 100L
-n_y = 50L
-
-stopifnot(ncol(wt_mat) == nrow(wt_mat))
-stopifnot(length(n_samps) == nrow(wt_mat))
-n_types <- length(n_samps)
-type_wts <- rep(1, n_types)
-total_pts <- n_x * n_y
-out_list <- rep(list(rep(list(numeric()), n_y)), n_x)
-neigh_diffs <- cbind(x = rep(-1:1, 3), y = rep(-1:1, each = 3))
-coll_samps <- rep(0L, n_types)
-wts <- rep(0, n_types)
-probs <- rep(0, n_types)
-#' `m` samples to take
-#' `n` total number of sampling options
-#' `i` signifies the (0-based) index of the current element
-#' `k` counts the number of elements already selected for the sample.
-p_fun <- function(m, n, i, k) {
-    return((m - k) / (n - i))
-}
-
-
-# Just for testing:
-out_list[[1]][[1]] <- 1
-out_list[[1]][[2]] <- 2
-out_list[[1]][[3]] <- 1:2
-out_list[[2]][[3]] <- 2
-
-
-# for (i in 1:n_x) {
-#     for (j in 1:n_y) {
-i = 2; j = 2
-
-
-
-for (k in sample.int(n_types)) {
-    probs[k] <- p_fun(n_samps[k], total_pts, (i-1L) * n_y + j - 1L, coll_samps[k])
-    wts[k] <- 1
-    for (id in -1:1) for (jd in -1:1) {
-        ii <- i + id
-        if (ii < 1 || ii > n_x) next
-        jj <- j + jd
-        if (jj < 1 || jj > n_y) next
-        nk_vec <- out_list[[ii]][[jj]]
-        if (length(nk_vec) == 0) next
-        for (nk in nk_vec) wts[k] <- wts[k] * wt_mat[k, nk]
-    }
-}
-
-probs
-# wts <- wts / mean(wts)
-
-probs |> sum()
-(probs * wts / mean(wts)) |> sum()
-(probs * wts / sum(wts)) |> sum()
-
-
-probs |> prod()
-(probs * wts / mean(wts)) |> prod()
-(probs * wts / sum(wts)) |> prod()
-
-
-
-
-
-
-n_x <- 50
-n_y <- 20
-xy_mat <- expand.grid(x = 1:n_x, y = 1:n_y)
-n <- n_x * n_y
-dmat <- matrix(0, n, n)
-# Takes ~ 9 sec with n=1000
-for (i in 1:(n-1)) for (j in (i+1):n) {
-    d <- sqrt((xy_mat[i,1] - xy_mat[j,1])^2 + (xy_mat[i,2] - xy_mat[j,2])^2)
-    dmat[i,j] <- dmat[j,i] <- d
-}; rm(i, j)
-
-Sigma <- 1 / dmat^3
-diag(Sigma) <- 3
-ev <- eigen(Sigma, symmetric = TRUE, only.values = TRUE)$values
-all(ev >= -1e-6 * abs(ev[1L])) # needs to be TRUE
-
-# Takes ~1.5 sec
-rn <- MASS::mvrnorm(1, rep(0, n), Sigma)
-
-
-Rcpp::cppFunction(
-"double pnorm0(const double& x, const double& s) {
-    double y = 0.5 * erfc(-1 * x / (s * M_SQRT2));
-    return y;
-}"
-)
-
-
-for (i in 1:1000) {
-    xx <- rnorm(1)
-    ss <- runif(1, 0, 2)
-    if (!all.equal(pnorm0(xx, ss), pnorm(xx, sd = ss))) stop("ugh")
-}; cat("YAY\n"); rm(i, xx, ss)
-
-
-ru <- pnorm(rn, rep(0, n), sqrt(diag(Sigma)))
-
-# hist(rn)
-# hist(ru)
-
-p <- c(0.4, 0.4, 0.1)
-cs_p <- cumsum(p)
-rt <- rep(length(p)+1, n)
-rt[ru <= cs_p[1]] <- 1
-rt[ru > cs_p[1] & ru <= cs_p[2]] <- 2
-rt[ru > cs_p[2] & ru <= cs_p[3]] <- 3
-table(rt) / n
-
-
-xy_mat |>
-    as_tibble() |>
-    mutate(type = factor(rt)) |>
-    ggplot(aes(x, y, fill = type)) +
-    geom_raster() +
-    scale_fill_viridis_d()
-
-xy_mat |>
-    as_tibble() |>
-    mutate(type = ru) |>
-    ggplot(aes(x, y, fill = type)) +
-    geom_raster() +
-    scale_fill_viridis_c()
-
-wts <- dmat
-diag(wts) <- 0
-
-ape::Moran.I(rn, wts)
-
-
-library(spdep)
-
-samp_list <- xy_mat |>
-    as.matrix() |>
-    knearneigh(k = 8) |>
-    knn2nb() |>
-    nb2listw(style="B")
-
-joincount.multi(factor(rt), samp_list)
-
-
-
-
-
-
-# coll_samps[k] <- coll_samps[k] - 1L
-
-
-neigh_mat <- t(matrix(c(i, j), 2, 8)) + neigh_diffs
-neigh_mat <- neigh_mat[neigh_mat[,1] > 0 & neigh_mat[,2] > 0, ]
-neigh_mat <- neigh_mat[neigh_mat[,1] <= n_x & neigh_mat[,2] <= n_y, ]
-neigh_mat
-
-#     }
-# }; rm(i, j)
-
-
-
-
-
 # color palette for target types:
 type_pal <- c(viridis(3, begin = 0.1, end = 0.9), "gray80")[c(2,1,4,3)] |>
     set_names(c("virus", "pseudo", "none", "both"))
@@ -207,16 +25,11 @@ xs <- ys <- 134L  # approximates a hectare with 0.75 m plant spacing
 #' Additionally, there should be a max of 4 types: `"virus"`, `"pseudo"`,
 #' `"none"`, or `"both"` (in that order).
 target_sims <- target_type_sims(xs+1L, ys+1L, #<< bc it sims locations 1:(*_size-1)
-                                # n_lands = 100,
-                                # n_threads = 1,
                                 wt_mat = rbind(c(1, 1), c(1, 1)),
                                 n_samples = rep(round(xs*ys * 0.5), 2)) |>
     getElement(1) |>
-    mutate(type = type |>
-               map_int(\(x) {
-                   if (length(x) == 1L) return(x)
-                   else return(4L)
-               }),
+    mutate(type = factor(type, levels = c("1", "2", "", "1_2")) |>
+               as.integer(),
            #' Same but with no bacteria
            #' This means that `"pseudo"` is changed to `"none"` and
            #' `"both"` to `"virus"`
@@ -239,6 +52,10 @@ target_sims <- target_type_sims(xs+1L, ys+1L, #<< bc it sims locations 1:(*_size
     t1 <- Sys.time()
     print(t1 - t0); rm(t0, t1)
 }
+
+
+#
+
 
 
 
