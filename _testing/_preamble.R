ef65d65c637a8916ae314bcc6318b65eaadcecfb
Lucas Nell <lucnell@gmail.com>
2025-06-25 15:17:25 -0400

    added futures to testing preamble

--- _testing/_preamble.R
+++ _testing/_preamble.R
@@ -3,13 +3,20 @@
 suppressPackageStartupMessages({
     library(tidyverse)
     library(pseudogameofclones)
+    library(future.apply)
+    library(progressr)
     library(patchwork)
     library(viridisLite)
     library(ggtext)
 })
 
-# number of threads:
+# number of threads (used not just for futures):
 .n_threads <- max(1L, parallel::detectCores() - 2L)
+
+plan(multisession, workers = .n_threads)
+handlers(global = TRUE)
+handlers("progress")
+
 
 
 #' This only gets run if my local .Rprofile has been run and if it's an
@@ -26,3 +33,5 @@ if (interactive() && exists("LAN_USER")) {
 # color palette for plant types:
 type_pal <- c(viridis(3, begin = 0.1, end = 0.9), "gray80")[c(2,1,4,3)] |>
     set_names(c("virus", "pseudo", "none", "both"))
+
+
